c7a7159563b17d10cf53e1e8497563208a9f0020
Zali Perlow <zali.perlow@flatironschool.com>
2018-01-05 00:06:26 -0500

    added Google maps and styling

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
 import  { connect } from 'react-redux'
 import Chart from '../components/chart'
+import GoogleMap from '../components/google_map'
 
 class WeatherList extends Component {
 
   renderWeather = (cityData) =>  {
 
-    const cityName = cityData.city.name;
+    const name = cityData.city.name;
     const temps = cityData.list.map(weather => weather.main.temp)
     const pressures = cityData.list.map(weather => weather.main.pressure)
     const humidities = cityData.list.map(weather => weather.main.humidity)
+    const { lon, lat } = cityData.city.coord;
 
     return (
-      <tr key={cityName}>
-        <td>{cityName}</td>
+      <tr key={name}>
+        <td><GoogleMap lon ={lon} lat={lat} /></td>
         <td><Chart data={temps} color="orange" units="K" /></td>
         <td><Chart data={pressures} color="green" units="hPa" /></td>
         <td><Chart data={humidities} color="black" units="%" /></td>
@@ -26,7 +28,7 @@ class WeatherList extends Component {
       <table className="table table-hover">
         <thead>
           <tr>
-            <th>City</th>
+            <th>City name</th>
             <th>Temperature (K)</th>
             <th>Pressure (hPa)</th>
             <th>Humidity (%)</th>
